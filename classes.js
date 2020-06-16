b64c2bbaace6eb0a9cd098d226f662613ac2ef1f
Jon-Erik Nissen <jenissen@me.com>
2020-06-16 14:44:11 -0700

    initial commit

--- classes.js
+++ classes.js
@@ -29,7 +29,20 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee{
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget = function(){
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+}
+
+let newEmployee = new Employee('JE', 'Nissen', 'je@je.com', 31);
+
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +60,20 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee{
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age);
+    this.reports = [];
+  }
+  hire = function(employee){
+    this.reports.push(employee)
+  }
+  fire = function(index){
+    this.reports.splice(index,1)
+  }
+}
+
+  
 
 
 ////////// PROBLEM 3 //////////
@@ -71,7 +97,33 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager{
+  constructor(first_name, last_name, email, age, reports){
+    // properties
+    super(first_name, last_name, email, age, reports);
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+
+  const hire = function(employee){
+    if(this.reports.length===0){
+      this.title = 'Not a manager'}
+    else if(this.reports.length>=1  && this.reports.length<=3  ){
+      this.title = "Bareley Manager"}
+    else if(this.reports.length>=4  && this.reports.length<=10 ){
+      this.title = "Mostly Manager"}
+    else if(this.reports.length>=11 && this.reports.length<=50 ){
+      this.title = "Manager"}
+    else if(this.reports.length>=51 && this.reports.length<=100){
+      this.title = "Bestest Manager"}
+      else if(this.reports.length>=101){
+        this.title = "Bestest Manager"}
+    }
+   const fire = function(employee){
+    // super(fire(employee));
+    this.bonus += 100;
+  }
+}
 
 
 
@@ -99,5 +151,3 @@
 */
 
 //Code Here
-
-
